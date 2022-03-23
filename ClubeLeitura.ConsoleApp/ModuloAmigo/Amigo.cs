8a7faab0eebb5fd7758603b1443cbdcb3ba45f3f
User-Wellengton <wellengtonjm@gmail.com>
2022-03-23 15:30:33 -0300

    Add project files.

---
+++ ClubeLeitura.ConsoleApp/ModuloAmigo/Amigo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClubeLeitura.ConsoleApp.ModuloAmigo
+{
+    public class Amigo
+    {
+        public string nome;
+        public string nomeResponsalvel;
+        public string telefone;
+        public string endereço;
+        public int numero;
+
+    }
+}
